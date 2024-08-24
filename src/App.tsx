f84f05968b566f4eab352400694f8530ceef6b4f
unknown <frontendvision3@gmail.com>
2024-08-24 11:38:16 +0300

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import BlogPreviewCard from "./components/BlogPreviewCard";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div className="app">
+      <BlogPreviewCard />
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+      colors: {
+        yellow: "hsl(47, 88%, 63%)",
+        white: "hsl(0, 0%, 100%)",
+        gray500: "hsl(0, 0%, 42%)",
+      },
+      boxShadow: {
+        shadow1: "8px 8px",
+      },
+    },
   },
   plugins: [],
 };
